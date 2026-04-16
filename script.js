065cbea1d16ff57c0407c1e25b2b2ec03d6ee3ff
azraelmauck-art <azraelmauck@gmail.com>
2026-04-16 17:46:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const gallery = document.getElementById("gallery");
   const box = document.querySelector(".box");
   const toggleButton = document.getElementById("toggleImage");
-  const toggleDarkmode = document.getElementById("toggleDarkmode")
+  const button = document.getElementById("toggleDarkmode")
+
+let darkMode = false;
  
+if (darkMode) {
+  document.body.style.background = "black";
+  document.body.style.color = "white";
+} else {
+  document.body.style.background = "white";
+  document.body.style.color = "black";
+}
+  
   let toggled = false;
   toggleButton.addEventListener("click", () => {
     if (toggled) {
