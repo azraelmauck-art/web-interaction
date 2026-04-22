85ea00ee72628ba4b972cc8897eb05da93302324
azraelmauck-art <azraelmauck@gmail.com>
2026-04-22 16:00:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const gallery = document.getElementById("gallery");
   const box = document.querySelector(".box");
-  const toggleButton = document.getElementById("toggleImage");
-  const button = document.getElementById("toggleDarkmode")
+  let image = document.getElementById("image");
+  const colorButton = document.getElementById("changeColor");
 
-let darkMode = false;
- 
-if (darkMode) {
-  document.body.style.background = "black";
-  document.body.style.color = "white";
-} else {
-  document.body.style.background = "white";
-  document.body.style.color = "black";
-}
-  
-  let toggled = false;
-  toggleButton.addEventListener("click", () => {
-    if (toggled) {
-      gallery.src = "./assets/images/formosan-ferret-badger.png";
-      toggled = false;
-    } else {
-      gallery.src = "./assets/images/marbled-polecat.png"; 
-      toggled = true;
-    }
+  colorButton.addEventListener("click", () => {
+    box.style.backgroundColor =
+    box.style.backgroundColor === "lightgreen" ? "brown" : "lightgreen"; 
   });
+
+image.addEventListener('mouseover', function(){
+  image.src = "./assets/images/formosan-ferret-badger.png"
+})
+image.addEventListener('mouseout', function(){
+    image.src = "./assets/images/marbled-polecat.png"
+  })
 
   console.log("Page loaded!")
 });
